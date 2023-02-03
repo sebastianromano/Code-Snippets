715e2916c4b2156a53be10a472bb5cc736552038
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2023-02-03 23:29:52 +0100

    formatting

--- Obsidian Web Clipper.js
+++ Obsidian Web Clipper.js
@@ -1,7 +1,11 @@
 /**
  * @name Obsidian Web Clipper (Apple platforms)
- * @desc This script will generate a new note in Obsidian containing the content of the current webpage. It only works on Apple devices. It utilizes Obsidian's [callout](https://help.obsidian.md/How+to/Use+callouts) and [tags](https://help.obsidian.md/How+to/Working+with+tags) syntax.
- * I've been using this since forever so it is hard to trace back the original source, but it looks like I've forked the script from [Stephan "Kepano" Ango's gist](https://gist.github.com/kepano/90c05f162c37cf730abb8ff027987ca3) and over time modified it to work better with my exact workflow.
+ * @desc This script will generate a new note in Obsidian containing the content of the current webpage.
+ * It utilizes Obsidian's [callout](https://help.obsidian.md/How+to/Use+callouts)
+ * and [tags](https://help.obsidian.md/How+to/Working+with+tags) syntax.
+ * I've been using this since forever so it is hard to trace back the original source,
+ * but it looks like I've forked the script from [Stephan "Kepano" Ango's gist](https://gist.github.com/kepano/90c05f162c37cf730abb8ff027987ca3)
+ * and over time modified it to work better with my exact workflow.
  */
 
 javascript: Promise.all([
@@ -9,8 +13,7 @@ javascript: Promise.all([
   import("https://unpkg.com/@tehshrike/readability@0.2.0"),
 ]).then(async ([{ default: Turndown }, { default: Readability }]) => {
   const vault = "Jarvis";
-  const folder =
-    "📖 Bookshelf/Unread/";
+  const folder = "📖 Bookshelf/Unread/";
   const tags = "- article\n- status_unread";
   function getSelectionHtml() {
     var html = "";
@@ -79,7 +82,11 @@ javascript: Promise.all([
     "\n" +
     "---\n" +
     "> [!info]-\n" +
-    "> This article was downloaded from the internet ([source](" + document.URL + ")) and placed on my [[Bookshelf]] on [[" + today + "]].\n\n" +
+    "> This article was downloaded from the internet ([source](" +
+    document.URL +
+    ")) and placed on my [[Bookshelf]] on [[" +
+    today +
+    "]].\n\n" +
     "> [!cite] Current progress\n" +
     "> - [ ] Mark as Read\n\n" +
     markdownBody;
